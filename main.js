c374e38aa57188a7876eb47e6619f9e24d588d34
Erik <elv180000@utdallas.edu>
2020-04-17 20:47:00 -0500

    Created a section for datastructures

--- main.js
+++ main.js
@@ -6,34 +6,6 @@ document.body.appendChild(renderer.view);
 stage.addChild(graphics);
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-class Queue {
-    constructor() {
-        this.items = [];
-    }
-    enqueue(element) {
-        this.items.push(element);
-    }
-    dequeue() {
-        if (this.isEmpty())
-            return "Underflow";
-        return this.items.shift();
-    }
-    front() {
-        if (this.isEmpty())
-            return "No elements in Queue";
-        return this.items[0];
-    }
-    isEmpty() {
-        return this.items.length == 0;
-    }
-    printQueue() {
-        var str = "";
-        for (var i = 0; i < this.items.length; i++)
-            str += this.items[i] + " ";
-        return str;
-    }
-}
-
 var yvel;
 var nodes;
 var screenWidth, screenHeight;
@@ -274,6 +246,26 @@ function isRunning() {
     return (dijSelect || astarSelect || bfSelect || swarmSelect);
 }
 
+/*########################### [ Supporting Data Structures ] ##################################*/
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    enqueue(element) {
+        this.items.push(element);
+    }
+    dequeue() {
+        if (this.isEmpty())
+            return "Underflow";
+        return this.items.shift();
+    }
+    isEmpty() {
+        return this.items.length == 0;
+    }
+}
+
+/*################################# [ Controls ] ####################################### */
 window.addEventListener('mousedown', function (e) {
     if (e.y > controlHeight) {
         hoveredNode = nodes[Math.floor(e.x / nodeSize)][Math.floor((e.y - controlHeight) / nodeSize)];
